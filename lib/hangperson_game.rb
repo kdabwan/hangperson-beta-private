3da2fbae8648c015babc77411fbecf884e384ddf
Kamel <kdabwan@berkeley.edu>
2015-02-04 16:23:38 -0800

    first commit

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -12,4 +12,21 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  attr_accessor :word, :guesses, :wrong_guesses
+  def initialize(new_word)
+  	@word = new_word
+  	@guesses = ''
+  	@wrong_guesses = ''
+  end
+
+  def guess(new_guess) 
+    raise ArgumentError if new_guess == nil || new_guess.length == 0 || !(/[[:alpha:]]/ === new_guess) 
+    return unless new_guess.length == 1
+    if (@word[new_guess]) 
+      @guesses[new_guess]? false : @guesses << new_guess 
+    else
+      @wrong_guesses[new_guess] ? false : @wrong_guesses << new_guess 
+    end 
+  end
+
 end
