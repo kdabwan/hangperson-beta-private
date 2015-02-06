d4b4374443b496a6bbb38a7612cdb0aef2b05119
Kamel <kdabwan@berkeley.edu>
2015-02-06 11:33:14 -0800

    Tests and scenarios passes

--- app.rb
+++ app.rb
@@ -40,6 +40,11 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    if letter.nil?
+      flash[:message] = "Invalid guess." 
+    else 
+      (@game.wrong_guesses[letter] || @game.guesses[letter])? flash[:message] = "You have already used that letter." : @game.guess(letter)
+    end
     redirect '/show'
   end
   
@@ -50,17 +55,17 @@ class HangpersonApp < Sinatra::Base
   # @wrong_guesses and @word_with_guesses, so set those up here.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    @game.check_win_or_lose == :play ? (erb :show): (erb @game.check_win_or_lose)
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    redirect '/show' # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    redirect '/show' # You may change/remove this line
   end
   
 end
